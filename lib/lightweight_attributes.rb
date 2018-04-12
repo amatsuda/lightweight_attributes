3e7f3d66084cc161630aa5cc698268396c21a3fb
Akira Matsuda <ronnie@dio.jp>
2018-04-13 08:36:56 +0900

    :put_litter_in_its_place:

--- lib/lightweight_attributes.rb
+++ lib/lightweight_attributes.rb
@@ -1,6 +1,5 @@
 # frozen_string_literal: true
 
-require "lightweight_attributes/version"
 require_relative 'lightweight_attributes/railtie'
 require_relative 'lightweight_attributes/base_class_methods'
 require_relative 'lightweight_attributes/attribute_set'
