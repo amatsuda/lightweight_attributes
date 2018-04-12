4413888696a02107dceeaafa8802e49630a5db87
Akira Matsuda <ronnie@dio.jp>
2018-04-12 20:36:49 +0900

    A super simple AttributeSet that delegates almost everything to a composed Hash instance

---
+++ lib/lightweight_attributes/attribute_set.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+require_relative 'attribute_set/builder'
+
+module LightweightAttributes
+  class AttributeSet
+    delegate :each_value, :fetch, :except, :[], :[]=, :key?, :keys, to: :attributes
+
+    def initialize(attributes)
+      @attributes = attributes
+    end
+
+    def fetch_value(name)
+      self[name]
+    end
+
+    private
+
+    attr_reader :attributes
+  end
+end
