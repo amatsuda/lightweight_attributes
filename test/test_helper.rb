5da2b67a12050ece67c1fb320800392a74bc097d
Akira Matsuda <ronnie@dio.jp>
2018-04-13 09:57:52 +0900

    The testing app

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,4 +1,20 @@
+# frozen_string_literal: true
+
 $LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
+require 'rails'
 require "lightweight_attributes"
+require 'active_record'
+require 'active_record/railtie'
 
 require "minitest/autorun"
+
+ENV['DATABASE_URL'] = 'sqlite3::memory:'
+
+module TestApp
+  Application = Class.new(Rails::Application) do
+    config.eager_load = false
+    config.active_support.deprecation = :log
+  end.initialize!
+end
+
+class Post < ActiveRecord::Base; end
