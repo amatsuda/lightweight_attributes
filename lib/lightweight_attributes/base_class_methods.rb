4efa2d251f1cb200901bd77eb81b5f418262d17c
Akira Matsuda <ronnie@dio.jp>
2019-06-19 10:30:56 +0900

    Drop default handling for now

--- lib/lightweight_attributes/base_class_methods.rb
+++ lib/lightweight_attributes/base_class_methods.rb
@@ -4,15 +4,6 @@ require_relative 'base_methods'
 
 module LightweightAttributes
   module BaseClassMethods
-    def _default_attributes # :nodoc:
-      load_schema
-      @default_attributes ||= if attributes_to_define_after_schema_loads.empty?
-        LightweightAttributes::AttributeSet.new({})
-      else
-        ActiveModel::AttributeSet.new({})
-      end
-    end
-
     def attributes_builder
       if attributes_to_define_after_schema_loads.empty?
         unless defined?(@attributes_builder) && @attributes_builder
@@ -20,17 +11,6 @@ module LightweightAttributes
           @attributes_builder = LightweightAttributes::AttributeSet::Builder.new(attribute_types, defaults)
         end
         @attributes_builder
-      else
-        super
-      end
-    end
-
-    #TODO: maybe we need to properly handle other non-nil values
-    private def define_default_attribute(name, value, type, from_user:)
-      super if attributes_to_define_after_schema_loads.any?
-
-      if value.nil? || (value == ActiveRecord::Attributes::ClassMethods.const_get(:NO_DEFAULT_PROVIDED))
-        _default_attributes[name] = nil
       else
         super
       end
