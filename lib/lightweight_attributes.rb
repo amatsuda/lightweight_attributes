0ea03f4f87938edcd96d0580e12adca8ccc570e0
Akira Matsuda <ronnie@dio.jp>
2019-06-22 17:07:06 +0900

    require base_class_methods after AR is loaded

--- lib/lightweight_attributes.rb
+++ lib/lightweight_attributes.rb
@@ -1,5 +1,4 @@
 # frozen_string_literal: true
 
 require_relative 'lightweight_attributes/railtie'
-require_relative 'lightweight_attributes/base_class_methods'
 require_relative 'lightweight_attributes/attribute_set'
--- lib/lightweight_attributes/railtie.rb
+++ lib/lightweight_attributes/railtie.rb
@@ -3,6 +3,8 @@
 module LightweightAttributes
   class Railtie < ::Rails::Railtie
     initializer 'lightweight_attributes' do
+      require_relative 'base_class_methods'
+
       ActiveSupport.on_load :active_record do
         extend LightweightAttributes::BaseClassMethods
         prepend LightweightAttributes::BaseMethods
