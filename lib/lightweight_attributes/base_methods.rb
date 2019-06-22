0fbac81cf34b5efc342726da039157d3b2a9f4ac
Akira Matsuda <ronnie@dio.jp>
2019-06-22 17:12:29 +0900

    AR 5.0 compat

--- lib/lightweight_attributes/base_methods.rb
+++ lib/lightweight_attributes/base_methods.rb
@@ -2,7 +2,7 @@
 
 module LightweightAttributes
   module BaseMethods
-    if (::ActiveRecord::VERSION::MAJOR == 5) && (::ActiveRecord::VERSION::MINOR == 1)
+    if (::ActiveRecord::VERSION::MAJOR == 5) && (::ActiveRecord::VERSION::MINOR < 2)
       def write_attribute(*)
         if LightweightAttributes::AttributeSet === @attributes
           @attributes = self.class.attributes_builder.build_original_from_database @attributes.raw_attributes, @attributes.additional_types
